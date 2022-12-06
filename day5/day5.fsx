5184958940e4853815410bf64e7b577c24de681d
Sebastian Siebert <4079623+SebastianSiebert@users.noreply.github.com>
2022-12-06 15:18:54 +0100

    day5 - reworking

--- day5/day5.fsx
+++ day5/day5.fsx
@@ -5,40 +5,49 @@ type StackDefinition = { Position: int; Index: int }
 
 type Stack = { Position: int; Index: int; Stack: string list}
 
-type Input =
-    | Stack of (int * string) list
-    | StackNumber of StackDefinition list
-    | Moves of Move
-    | Empty
     
-let readFile filePath = System.IO.File.ReadLines filePath
-let regexStack = Regex "([A-Z])"
-let regexStackNo = Regex "(\d)"
-let regexMoves = Regex "move (\d+) from (\d+) to (\d+)"
+module InputHandler =
+    type Input =
+        | Stack of (int * string) list
+        | StackNumber of StackDefinition list
+        | Moves of Move
+        | Empty
 
-let matchMove (moveMatch: Match) = Moves {Number=int moveMatch.Groups[1].Value; FromStack=int moveMatch.Groups[2].Value; ToStack=int moveMatch.Groups[3].Value}
-let matchStackNo (stackNoMatch: MatchCollection) =
-    let mapStackNo index =
-        let item = stackNoMatch.Item index
-        { Position = item.Index; Index = int item.Value }
-    let l = [0..stackNoMatch.Count-1] |> List.map mapStackNo
-    StackNumber l
-let matchStack (stackMatch: MatchCollection) =
-    let mapStack index =
-        let item = stackMatch.Item index
-        (item.Index, item.Value)
-    let l = [0..stackMatch.Count-1] |> List.map mapStack
-    Stack l
-
-let mapValues line =
-    let move = regexMoves.Match line
-    let stackNo = regexStackNo.Matches line
-    let stack = regexStack.Matches line
-    if move.Success then matchMove move
-    elif stackNo.Count > 0 then  matchStackNo stackNo
-    elif stack.Count > 0 then matchStack stack
-    else Empty
+    let (|IsMoves|) input = match input with | Moves m -> Some m | _ -> None
+    let (|IsStackNumber|) input = match input with | StackNumber sn -> Some sn | _ -> None
+    let (|IsStack|) input = match input with | Stack s -> Some s | _ -> None
+    let chooseInput (|IsInput|_|) value = match value with | IsInput t -> Some t | _ -> None
     
+    let readFile filePath = System.IO.File.ReadLines filePath
+    let regexStack = Regex "([A-Z])"
+    let regexStackNo = Regex "(\d)"
+    let regexMoves = Regex "move (\d+) from (\d+) to (\d+)"
+
+    let matchMove (moveMatch: Match) = Moves {Number=int moveMatch.Groups[1].Value; FromStack=int moveMatch.Groups[2].Value; ToStack=int moveMatch.Groups[3].Value}
+    let matchStackNo (stackNoMatch: MatchCollection) =
+        let mapStackNo index =
+            let item = stackNoMatch.Item index
+            { Position = item.Index; Index = int item.Value }
+        let l = [0..stackNoMatch.Count-1] |> List.map mapStackNo
+        StackNumber l
+    let matchStack (stackMatch: MatchCollection) =
+        let mapStack index =
+            let item = stackMatch.Item index
+            (item.Index, item.Value)
+        let l = [0..stackMatch.Count-1] |> List.map mapStack
+        Stack l
+
+    let mapValues line =
+        let move = regexMoves.Match line
+        let stackNo = regexStackNo.Matches line
+        let stack = regexStack.Matches line
+        if move.Success then matchMove move
+        elif stackNo.Count > 0 then  matchStackNo stackNo
+        elif stack.Count > 0 then matchStack stack
+        else Empty
+    
+open InputHandler
+
 let createEmptyStacks (stackDefinition: StackDefinition list) =
     let stacks = stackDefinition |> List.fold (fun state def -> {Position=def.Position; Index=def.Index; Stack=[]}::state) []
     stacks
@@ -62,12 +71,11 @@ let makeMoves fn stacks move =
         | _ -> s
     stacks |> List.map mapStack
     
-    
-let processMoves makeMovesFn stack = Seq.choose (fun e -> match e with | Moves m -> Some m | _ -> None) >> Seq.fold (makeMoves makeMovesFn) stack >> Seq.sortBy (fun s -> s.Index) >> Seq.map (fun e -> e.Stack.Head) >> System.String.Concat
-
 let inputs = "input.txt" |> readFile |> Seq.map mapValues
-let stackDefinition = inputs |> Seq.choose (fun e -> match e with | StackNumber sn -> Some sn | _ -> None) |> Seq.head
-let stacks = inputs |> Seq.choose (fun e -> match e with | Stack s -> Some s | _ -> None) |> Seq.rev |> Seq.fold fillStacks (createEmptyStacks stackDefinition)
+let stackDefinition = inputs |> Seq.choose (chooseInput InputHandler.(|IsStackNumber|)) |> Seq.head
+let stacks = inputs |> Seq.choose (chooseInput InputHandler.(|IsStack|)) |> Seq.rev |> Seq.fold fillStacks (createEmptyStacks stackDefinition)
+
+let processMoves makeMovesFn stack = Seq.choose (chooseInput InputHandler.(|IsMoves|)) >> Seq.fold (makeMoves makeMovesFn) stack >> Seq.sortBy (fun s -> s.Index) >> Seq.map (fun e -> e.Stack.Head) >> System.String.Concat
 
 inputs |> processMoves List.rev stacks |> printfn "Part1 = %s"
 inputs |> processMoves id stacks |> printfn "Part2 = %s"
