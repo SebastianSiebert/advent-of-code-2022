185b0736c9717eb7ed92dcd4eb36cb26431d7108
Sebastian Siebert <4079623+SebastianSiebert@users.noreply.github.com>
2022-12-06 07:26:21 +0100

    day5 - initial solution (hardcoded stacks)

---
+++ day5/day5-hardcodedStack.fsx
@@ -0,0 +1,44 @@
+﻿open System.Text.RegularExpressions
+
+type Move = { Number: int; FromStack: int; ToStack: int }
+type Stack = {Index: int; Stack: string list}
+
+type Input =
+    | Moves of Move
+    | Empty
+    
+let exampleStack = [ {Index=1; Stack=["N";"Z"]}; {Index=2; Stack=["D";"C";"M"]}; {Index=3; Stack=["P"]} ]
+let inputStack = [
+    {Index=1; Stack=["P";"D";"Q";"R";"V";"B";"H";"F"]}
+    {Index=2; Stack=["V";"W";"Q";"Z";"D";"L"]}
+    {Index=3; Stack=["C";"P";"R";"G";"Q";"Z";"L";"H"]}
+    {Index=4; Stack=["B";"V";"J";"F";"H";"D";"R"]}
+    {Index=5; Stack=["C";"L";"W";"Z"]}
+    {Index=6; Stack=["M";"V";"G";"T";"N";"P";"R";"J"]}
+    {Index=7; Stack=["S";"B";"M";"V";"L";"R";"J"]}
+    {Index=8; Stack=["J";"P";"D"]}
+    {Index=9; Stack=["V";"W";"N";"C";"D"]}
+]
+    
+let readFile filePath = System.IO.File.ReadLines filePath
+let regexMoves = Regex "move (\d+) from (\d+) to (\d+)"
+
+let mapValues line =
+    let move = regexMoves.Match line
+    if move.Success then Moves {Number=int move.Groups[1].Value; FromStack=int move.Groups[2].Value; ToStack=int move.Groups[3].Value}
+    else Empty
+    
+let rec makeMoves fn stacks move =
+    let stack = List.find (fun e -> e.Index = move.FromStack) stacks
+    let mapStack s =
+        match s.Index with
+        | _ when s.Index = move.FromStack -> {s with Stack = s.Stack[move.Number..] }
+        | _ when s.Index = move.ToStack -> {s with Stack = (List.append (fn stack.Stack[..move.Number-1]) s.Stack)}
+        | _ -> s
+    stacks |> List.map mapStack
+    
+    
+let processData makeMovesFn stack = readFile >> Seq.map mapValues >> Seq.filter (fun e -> match e with | Moves _ -> true | _ -> false) >> Seq.map (fun (Moves m) -> m) >> Seq.fold (makeMoves makeMovesFn) stack >> Seq.map (fun e -> e.Stack.Head) >> System.String.Concat
+
+"input.txt" |> processData List.rev inputStack |> printfn "Part1 = %s" 
+"input.txt" |> processData id inputStack |> printfn "Part2 = %s"
---
+++ day5/day5.fsx
@@ -0,0 +1,56 @@
+﻿open System.Text.RegularExpressions
+
+type Move = { Number: int; FromStack: int; ToStack: int }
+
+type Input =
+    | Stack of (int * string) list
+    | StackNumber of (int * string) list
+    | Moves of Move
+    | Empty
+    
+let readFile filePath = System.IO.File.ReadLines filePath
+let regexStack = Regex "([A-Z])"
+let regexStackNo = Regex "(\d)"
+let regexMoves = Regex "move (\d+) from (\d+) to (\d+)"
+
+let matchMove (moveMatch: Match) =
+    Moves {Number=int moveMatch.Groups[1].Value; FromStack=int moveMatch.Groups[2].Value; ToStack=int moveMatch.Groups[3].Value}
+let matchStackNo (stackNoMatch: MatchCollection) =
+    let mapStackNo index =
+        let item = stackNoMatch.Item index
+        (item.Index, item.Value)
+    let l = [0..stackNoMatch.Count-1] |> List.map mapStackNo
+    StackNumber l
+let matchStack (stackMatch: MatchCollection) =
+    let mapStack index =
+        let item = stackMatch.Item index
+        (item.Index, item.Value)
+    let l = [0..stackMatch.Count-1] |> List.map mapStack
+    Stack l
+
+let mapValues line =
+    let move = regexMoves.Match line
+    let stackNo = regexStackNo.Matches line
+    let stack = regexStack.Matches line
+    if move.Success then matchMove move
+    elif stackNo.Count > 0 then  matchStackNo stackNo
+    elif stack.Count > 0 then matchStack stack
+    else Empty
+    
+type Stack = {Index: string; Stack: string list}
+
+let createStacks stackDefinition =
+    List.map (fun (_, value) -> {Index= value; Stack= []}) stackDefinition
+
+let mapStacks stackDefinition state stackRow =
+    
+    List.fold (fun e -> e)
+    state
+
+let inputs = "example.txt" |> readFile |> Seq.map mapValues
+let stackDefinition = inputs |> Seq.find (fun e -> match e with | StackNumber _ -> true | _ -> false) |> (fun (StackNumber e) -> e)
+
+let stacks = inputs |> Seq.filter (fun e -> match e with | Stack _ -> true | _ -> false)|> Seq.map (fun (Stack e) -> e) |> Seq.rev |> Seq.fold (mapStacks stackDefinition) (createStacks stackDefinition)
+let moves = inputs |> Seq.filter (fun e -> match e with | Moves _ -> true | _ -> false)
+
+printfn "%A" stackDefinition
