e21f038d289cf28c6d4895ab9975f7ed19bf2f61
Sebastian Siebert <4079623+SebastianSiebert@users.noreply.github.com>
2022-12-12 13:04:48 +0100

    day12 - some optimizations

--- day12/day12.fsx
+++ day12/day12.fsx
@@ -1,4 +1,6 @@
-﻿type Direction = | Right | Left | Up | Down
+﻿open System.Diagnostics
+
+type Direction = | Right | Left | Up | Down
 type Coordinates = { X: int; Y: int; Elevation: int; Position: bool; Goal: bool; Visited: bool  }
 let directions = [(-1,0);(1,0);(0,-1);(0,1)]
 
@@ -25,18 +27,30 @@ let mapPosition cp (x,y) pos =
             {pos with Position=true; Visited=true}
         | _ -> pos
         
+let posFilter coords cp (x,y) =
+    let positions = coords |> Seq.tryFind (fun e -> e.X = cp.X + x && e.Y = cp.Y + y && e.Elevation <= cp.Elevation + 1 && not e.Visited)
+    match positions with | None -> false | Some _ -> true
+        
 let rec traverseGrid coords results direction =
     let currentPos = coords |> tryFindPosition
     match currentPos with
     | None -> results
     | Some cp when cp.Goal -> (coords |> filterVisited |> Seq.length)::results
     | Some cp ->
-        let newCoords = coords |> Seq.map (mapPosition cp direction)
-        let visited = newCoords |> filterVisited |> Seq.length
-        match results |> List.tryFind (fun e -> visited >= e) with
-        | Some _ -> results
-        | None -> directions |> List.map (traverseGrid newCoords results) |> List.concat
+        let newPositions = directions |> List.filter (posFilter coords cp)
+        match newPositions.Length with
+        | length when length = 0 -> results
+        | _ ->
+            let newCoords = coords |> Seq.map (mapPosition cp direction)
+            let visited = newCoords |> filterVisited |> Seq.length
+            match results |> List.tryFind (fun e -> visited >= e) with
+            | Some _ -> results
+            | None -> directions |> List.map (traverseGrid newCoords results) |> List.concat
+        
 
-let coords = "../aoc2022-input/day12/input.txt" |> readFile |> Seq.mapi mapLines |> Seq.concat
+let coords = "../aoc2022-input/day12/input.txt" |> readFile |> Seq.mapi mapLines |> Seq.concat |> Seq.toList
 
+let stopwatch = Stopwatch.StartNew()
 directions |> List.map (traverseGrid coords []) |> List.concat |> List.min |> (fun visited -> visited - 1) |> printfn "Part1 = %i"
+stopwatch.Stop()
+printfn $"Elapsed: {stopwatch.Elapsed}"
